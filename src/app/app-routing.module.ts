c2bfb9a5ca722dfd11d5477e4a7babd5820e5f39
Accenture.Robotics <Accenture.Robotics@accenture.com>
2019-11-12 16:04:05 +0530

    basic configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DocumentationComponent } from './documentation/documentation.component';
 
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'documentation', component: DocumentationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
